5c4adcfdb696246bc94202a9f05b9ccfba0e3bce
romanzpolski <romankondratczyk@gmail.com>
2018-07-24 13:55:29 +0100

    some comments

--- classes/Controller.php
+++ classes/Controller.php
@@ -90,24 +90,30 @@ class Controller {
         $allFlights = array();
 
         $flightsClient = new flightsClient();
+
+        // get departure flights from API
         $res = $flightsClient->getAirlineFlightSchedules($icao, $limit, $offset);
 
         $airport = $flightsClient->getAirportInfo($icao);
         $metar = $flightsClient->getMetarEx($icao);
         $resWithFlights = $this->prepareDepartureFlights($res->data, $airport);
 
+        // add flights to all flights
         $allFlights = array_merge($allFlights, $resWithFlights);
 
+
+        // API return is max 100 items, so we need to paginate, limit 15
         $c=0;
         while( $res->next_offset > 0 && $c < 15 ) {
             $c++;
-            $offset = $offset + 100;
-            $res = $flightsClient->getAirlineFlightSchedules($icao, $limit, $offset);;
             $resFilteredToAdd = $this->filterByTimeFrame($res->data);
             $resWithAirlineToAdd = $this->prepareDepartureFlights($resFilteredToAdd, $airport);
             $allFlights = array_merge($allFlights, $resWithAirlineToAdd);
+            $res = $flightsClient->getAirlineFlightSchedules($icao, $limit, $offset);
+            $offset = $offset + 100;
         }
 
+        // form data array
         $data = array(
             'heading' => 'Departures from '.$airport->name,
             'subHeading'=> $airport->location.' | '.$metar->cloud_friendly.' | '.$metar->temp_air.'C | '.$metar->wind_friendly,
@@ -130,9 +136,8 @@ class Controller {
             $icaos = array();
             $flightsClient = new flightsClient();
 
+            // put data in object
             foreach($res as $f){
-
-
                 $flight = (object)[
                     'id'=> $f->ident,
                     'arr'=> $this->formatDates($f->arrivaltime),
@@ -146,15 +151,18 @@ class Controller {
                 $icaos[]= $f->destination;
             }
 
+            // call API for more details
             $owners = $flightsClient->getTailOwnersMulti($idents);
             $airports = $flightsClient->getAirportInfosMulti($icaos);
 
+            // add more data to $flights array
             foreach($flights as $i => $f){
                 $f->owner = $owners[$i]->TailOwnerResult;
                 $f->destination = $airports[$i]->AirportInfoResult->name;
                 $f->destinationName = $airports[$i]->AirportInfoResult->location;
             }
 
+            // sort $flights array by departure time ASC
             usort($flights, function($a, $b)
             {
                 $date1 = $a->dep;
@@ -185,12 +193,15 @@ class Controller {
         foreach($arr as $f){
             $ts = new DateTime();
             if(property_exists($f, 'estimatedarrivaltime')){
+                // in case of arrivals
                 $flightTS = $f->estimatedarrivaltime;
             } else {
+                // departures
                 $flightTS = $f->departuretime;
             }
             $ts = $ts->setTimestamp($flightTS);
             if($ts > $now && $ts < $timeLimit) {
+                // if records within timeframe
                 $data[]= $f;
             }
         }
